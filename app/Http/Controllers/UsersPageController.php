ca9e61e8fbbb8b49f365b4d32baa5754ea6941a4
MRPartik <loyolapat04@gmail.com>
2019-01-10 09:33:08 +0800

    usersmanagement

---
+++ app/Http/Controllers/UsersPageController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\r_affiliate_info;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UsersPageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $aff = r_affiliate_info::all();
+        return view('admin.users.affiliate',compact('aff'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $aff = new r_affiliate_info();
+        $aff->AFF_CODE = $request->code;
+        $aff->AFF_NAME = $request->affname;
+        $aff->AFF_PAYMENT_INSTRUCTION = $request->paymentinst;
+        $aff->AFF_PAYMENT_MODE = $request->paymentmode;
+        $aff->AFF_DESC = $request->desc;
+        $aff->save();
+
+        return redirect('admin/users/affiliate')->with('success','Affiliate record was inserted successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $aff = r_affiliate_info::all();
+        return  new JsonResource($aff->where('AFF_ID',$id));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+
+        $aff =  r_affiliate_info::where('AFF_ID',$id)->first();
+        $aff->AFF_CODE = $request->code;
+        $aff->AFF_NAME = $request->affname;
+        $aff->AFF_PAYMENT_INSTRUCTION = $request->paymentinst;
+        $aff->AFF_PAYMENT_MODE = $request->paymentmode;
+        $aff->AFF_DESC = $request->desc;
+        $aff->save();
+
+        return redirect('admin/users/affiliate')->with('success','Affiliate record was updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function actDeact(Request $request){
+
+        $tax = r_affiliate_info::where('AFF_ID',$request->id)->first();
+
+        if($request->type == 0){
+            $tax->AFF_DISPLAY_STATUS = 0;
+            $tax->save();
+            redirect('admin/users/affiliate')->with('success','Affiliate record was deactivated successfully');
+        }else if($request->type == 1){
+            $tax->AFF_DISPLAY_STATUS = 1;
+            $tax->save();
+            redirect('admin/users/affiliate')->with('success','Affiliate record was activated successfully');
+        }
+    }
+}
---
+++ resources/views/admin/users/affiliate.blade.php
@@ -0,0 +1,315 @@
+
+@extends('layouts.admin')
+
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1>
+                Affiliates Management
+                <small>Overview</small>
+            </h1>
+            <ol class="breadcrumb">
+                <li><a href="#"><i class="fa fa-shield"></i> Affiliates Management</a></li>
+            </ol>
+        </section>
+
+
+        <div class="modal modal-default fade" id="taxreferencesetup" >
+            <div class="modal-dialog">
+                <div class="box box-warning box-solid">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Affiliates Management</h3>
+                    </div>
+                    <div class="box-body">
+                        <form method="post" id="affModal" action="{{url('admin/users/affiliate')}}"  enctype="multipart/form-data">
+                            {{csrf_field()}}
+                            <input type="hidden" name="_method" value="POST" />
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label>Name</label>
+                                            <input class="form-control" name=affname placeholder="Name" required>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Code</label>
+                                            <input class="form-control" name=code type="text" placeholder="Code" required>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label>Payment Mode</label>
+                                            <input class="form-control" name=paymentmode type="text" placeholder="Payment Mode" required>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label>Payment Instruction</label>
+                                            <textarea class="form-control" name=paymentinst style="resize:vertical; width:100%;height:107px" placeholder="Payment Instruction" required></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label>Description</label>
+                                            <textarea class="form-control" name=desc style="resize:vertical; width:100%;height:107px" placeholder="Affiliate Description" required></textarea>
+                                        </div>
+                                    </div>
+                            <!-- /.row -->
+                                </div>
+                                <div class="col-md-12" >
+                                    <div class="pull-right" style="margin-right: 10px;">
+                                        <button class="btn btn-success" type="submit" >Save</button>
+                                        <a class="btn btn-danger" data-dismiss="modal"> Cancel</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div id="overlay" class="overlay" style="display:none">
+                        <i class="fa fa-refresh fa-spin"></i>
+                    </div>
+
+                </div>
+                <!-- /.modal-content -->
+            </div>
+            <!-- /.modal-dialog -->
+        </div>
+        <!-- /.modal -->
+
+
+        <!-- Main content -->
+        <section class="content">
+
+
+            <div class="box box-success box-solid">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Affiliates Setup</h3>
+
+                    <div class="box-tools pull-right">
+                        <a href="#taxreferencesetup" id="addTax" class="btn btn-success" data-toggle="modal"><i class="fa fa-plus"></i> Add Item</a>
+                    </div>
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body" >
+                    <div class="row">
+                        <div class="col-md-12">
+                            <table id="example2" class="table table-bordered table-hover">
+                                <thead>
+                                <tr>
+                                    <th style="width: 30%">Info</th>
+                                    <th>Code</th>
+                                    <th>Payment Mode</th>
+                                    <th>Date Issued</th>
+                                    <th>Action</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($aff as $item)
+                                    <tr>
+                                        <td><strong>{{ $item->AFF_NAME }}</strong><br><i>{{ $item->AFF_COD }}</i> </td>
+                                        <td>{{$item->AFF_CODE}}</td>
+                                        <td>{{ $item->AFF_PAYMENT_MODE   }}</td>
+                                        <td>{{ (new DateTime($item->created_at))->format('D M d, Y | h:i A') }}</td>
+                                        <td>
+                                            <center>
+                                                @if($item->AFF_DISPLAY_STATUS ==1)
+                                                    <a class="btn btn-info" id='editTax'data-toggle="modal" vals="{{$item->AFF_ID }}" href="#taxreferencesetup"><i class="fa fa-pencil"></i></a>
+                                                    <a id=deact  vals="{{$item->AFF_ID }}" class="btn btn-danger" data-toggle="modal" data-target="#deactivate"><i class="fa fa-ban"></i></a>
+                                                @else
+                                                    <a id=act  vals="{{$item->AFF_ID }}" class="btn btn-success" data-toggle="modal" data-target="#activate"><i class="fa fa-rotate-left"></i></a>
+                                                @endif
+                                            </center>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                                <tfoot>
+                                <th style="width: 30%">Info</th>
+                                <th>Code</th>
+                                <th>Payment Mode</th>
+                                <th>Date Issued</th>
+                                <th>Action</th>
+                                </tfoot>
+                            </table>
+                        </div>
+                    </div>
+                    <!-- /.row -->
+                </div>
+                <!-- /.box-body -->
+                <div class="box-footer" >
+                </div>
+            </div>
+            <!-- /.box -->
+
+
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+
+
+
+@endsection
+
+@section('extrajs')
+    <script>
+
+        @if(session('success'))
+        iziToast.success({
+            title: 'OK',
+            position:"topRight",
+            message: '{{session('success')}}',
+        });
+        @elseif(session('error'))
+        iziToast.error({
+            title: 'Error',
+            position:"topRight",
+            message: '{{session('error')}}',
+        });
+        @endif
+        $("a[id='addTax']").on('click',function(){
+            document.querySelector('#affModal').reset();
+        });
+        $("a[id='editTax']").on('click',function () {
+            $('.modal-title').html('Editing Tax Reference');
+            document.querySelector('#affModal').reset();
+            $id = $(this).attr('vals');
+            $.ajax({
+                url: '/admin/users/affiliate/'+$id
+                ,type: 'get'
+                ,data: {_token:CSRF_TOKEN }
+                ,dataType:'json'
+                ,success:function($data){
+
+                    $("input[name='affname']").val($data.data[0].AFF_NAME);
+                    $("textarea[name='desc']").val($data.data[0].AFF_DESC);
+                    $("textarea[name='paymentinst']").val($data.data[0].AFF_PAYMENT_INSTRUCTION);
+                    $("input[name='paymentmode']").val($data.data[0].AFF_PAYMENT_MODE);
+                    $("input[name='code']").val($data.data[0].AFF_CODE);
+                    $('#affModal').attr('action','{{url('admin/users/affiliate')}}/'+$data.data[0].AFF_ID);
+                    $("input[name='_method']").attr('value','PATCH');
+                }
+                ,error:function(){
+
+                }
+            });
+
+        });
+
+        $('#example2').DataTable({
+            'paging'      : true,
+            'lengthChange': true,
+            'searching'   : true,
+            'ordering'    : true,
+            'info'        : true,
+            'autoWidth'   : true,dom: 'Bfrtip'
+            ,   buttons: [
+                { extend: 'copy', className: 'btn-sm',
+                    exportOptions: {
+                        columns: [0,1,2,3]
+                    }
+                },
+                { extend: 'csv', className: 'btn-sm' ,
+                    exportOptions: {
+                        columns: [0,1,2,3]
+                    }
+                },
+                { extend: 'excel', className: 'btn-sm',
+                    exportOptions: {
+                        columns: [0,1,2,3]
+                    }
+                },
+                { extend: 'pdf', className: 'btn-sm',
+                    exportOptions: {
+                        columns: [0,1,2,3]
+                    }
+                },
+                { extend: 'print', className: 'btn-sm',
+                    exportOptions: {
+                        columns: [0,1,2,3]
+                    }
+                },
+
+
+            ],
+        });
+
+
+        $("a[id='deact']").on('click',function(){
+            $id = $(this).attr('vals');
+            // $("button[id='deactSave']").on('click',function () {
+            swal({
+                title: "This record will be deactivated?"
+                , text: "After this action, this record is not available, unless it is activated"
+                , type: "warning"
+                , showLoaderOnConfirm: true
+                , showCancelButton: true
+                , confirmButtonColor: '#9DD656'
+                , confirmButtonText: 'Yes!'
+                , cancelButtonText: "No!"
+                , closeOnConfirm: false
+                , closeOnCancel: true
+            }, function (isConfirm) {
+                if (isConfirm) {
+
+                    $.ajax({
+                        url: '/affiliate/actDeact'
+                        ,type: 'post'
+                        ,data: {id:$id,_token:CSRF_TOKEN, type:0  }
+                        ,success:function(){
+                            window.location.reload();
+                        }
+                        ,error:function(){
+
+                        }
+                    });
+
+                }
+            });
+            // });
+        });
+
+        $("a[id='act']").on('click',function(){
+            $id = $(this).attr('vals');
+            // $("button[id='actSave']").on('click',function () {
+
+            swal({
+                title: "This record will be activated?"
+                , text: "After this action, this record is now available, unless it is deactivated"
+                , type: "warning"
+                , showLoaderOnConfirm: true
+                , showCancelButton: true
+                , confirmButtonColor: '#9DD656'
+                , confirmButtonText: 'Yes!'
+                , cancelButtonText: "No!"
+                , closeOnConfirm: false
+                , closeOnCancel: true
+            }, function (isConfirm) {
+                if (isConfirm) {
+                    $.ajax({
+                        url: '/affiliate/actDeact'
+                        ,type: 'post'
+                        ,data: {id:$id,_token:CSRF_TOKEN, type:1  }
+                        ,success:function(){
+                            window.location.reload();
+                        }
+                        ,error:function(){
+
+                        }
+                    });
+                }
+            });
+
+            // });
+        });
+
+    </script>
+@endsection
+
+
